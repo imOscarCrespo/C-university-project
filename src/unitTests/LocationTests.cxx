9aff361c09147b61dc363971d0a6518c347e90cc
Anders <wisdompoet@hotmail.com>
2018-01-25 17:18:43 +0100

    committed lab 2

---
+++ src/unitTests/LocationTests.cxx
@@ -0,0 +1,43 @@
+#include <MiniCppUnit.hxx>
+
+class LocationTest : public TestFixture< LocationTest >
+{
+public:
+	TEST_FIXTURE( LocationTest )
+	{
+	//	TEST_CASE( test_name_byDefault );
+	//	TEST_CASE( test_name_whenChanged );
+	//	TEST_CASE( test_description_whenEmpty );
+	}
+	/*
+	void test_name_byDefault()
+	{
+		Location location;
+		ASSERT_EQUALS(
+			"unknown",
+			location.name()
+		);
+	}
+	void test_name_whenChanged()
+	{
+		Location location;
+		location.name( "A location" );
+		ASSERT_EQUALS(
+			"A location",
+			location.name()
+		);
+	}
+	void test_description_whenEmpty()
+	{
+		Location location;
+		location.name( "A location" );
+		ASSERT_EQUALS(
+			"Location: A location\n",
+			location.description()
+		);
+	}
+	*/
+};
+
+REGISTER_FIXTURE( LocationTest )
+
